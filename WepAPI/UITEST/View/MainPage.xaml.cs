a5bf18d27fcbae480cd24fc68b873c4f517f4166
jehj <jehj@itu.dk>
2017-12-06 18:18:17 +0100

    Reset

--- WepAPI/UITEST/View/MainPage.xaml.cs
+++ WepAPI/UITEST/View/MainPage.xaml.cs
@@ -60,40 +60,8 @@ namespace UITEST
         private void Title_Click(object sender, RoutedEventArgs e)
         {
             var btn = sender as Button;
-            Post post = btn.DataContext as Post;
+            Post post = btn.Content as Post;
             Frame.Navigate(typeof(PostPage), post);
         }
-
-        private void VoteButton_Clicked(object sender, RoutedEventArgs e)
-        {
-            var btn = sender as Button;
-            var post = btn.DataContext as Post;
-            if (btn.Content.Equals("Like"))
-            {
-                post.NumOfVotes += 1;
-            }
-            else if (btn.Content.Equals("Dislike"))
-            {
-                post.NumOfVotes -= 1;
-            }
-        }
-
-
-        //hemmelig besked til jens
-
-        private void PostVoteButton_Clicked(object sender, RoutedEventArgs e)
-        {
-            var btn = sender as Button;
-            var post = btn.DataContext as Post;
-            if (btn.Content.Equals("Like"))
-            {
-                post.NumOfVotes += 1;
-            }
-            else if (btn.Content.Equals("Dislike"))
-            {
-                post.NumOfVotes -= 1;
-            }
-        }
-
     }
 }
