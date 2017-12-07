c0c24426f051b1c1c0d6c8d537d1949c5efa8320
Hjaltejuel <hjsk@itu.dk>
2017-12-07 15:24:33 +0100

    lets go

---
+++ WepAPI/UI.Test/AuthenticationTest.cs
@@ -0,0 +1,35 @@
+﻿
+using Gorilla.AuthenticationGorillaAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UI.Test
+{
+    public class AuthenticationTest
+    {
+        
+        private readonly IAuthenticationHelper _helper;
+        public AuthenticationTest(IAuthenticationHelper helper)
+        {
+            _helper = helper;
+        }
+
+        [Fact]
+        public async Task Authenticate()
+        {
+            var _account = await _helper.SignInAsync();
+            Assert.NotNull(_account);
+        }
+        
+        [Fact]
+        public void TestAll()
+        {
+            var d = 5;
+            Assert.Equal(5,d);
+        }
+    }
+}
