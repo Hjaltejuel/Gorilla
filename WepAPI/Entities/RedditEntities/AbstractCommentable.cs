77be280839a8ebd2b5bf7b0553ce4aeec55aa3c7
Mads Pedersen <madpe@itu.dk>
2017-12-12 11:31:09 +0100

    post og comment er blevet flotte (AbstractCommentable)

--- WepAPI/Entities/RedditEntities/AbstractCommentable.cs
+++ WepAPI/Entities/RedditEntities/AbstractCommentable.cs
@@ -7,6 +7,21 @@ namespace Entities.RedditEntities
         public ObservableCollection<Comment> Replies { get; set; }
         public int created_utc { get; set; }
         public string author { get; set; }
+        public int score { get; set; }
+        public string likes { get; set; }
+        public bool can_mod_post { get; set; }
+        public string name { get; set; }
+        public int downs { get; set; }
+        public int ups { get; set; }
+        public int created { get; set; }
+        public bool archived { get; set; }
+        public bool edited { get; set; }
+        public bool saved { get; set; }
+        public string report_reasons { get; set; }
+        public string approved_at_utc { get; set; }
+        public string banned_by { get; set; }
+        public string subreddit { get; set; }
+        public string[] user_reports { get; set; }
 
 
         public void BuildReplies(Listing listing)
