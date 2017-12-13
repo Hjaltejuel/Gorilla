bc0179f60c211781cbc815e936aec8b4e7465703
Jens <jehj@itu.dk>
2017-12-13 14:06:55 +0100

    asd

--- WepAPI/UITEST/ViewModel/CreatePostPageViewModel.cs
+++ WepAPI/UITEST/ViewModel/CreatePostPageViewModel.cs
@@ -1,30 +1,69 @@
 ﻿using Entities.RedditEntities;
+using Gorilla.AuthenticationGorillaAPI;
+using Gorilla.Model;
+using Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Popups;
 
 namespace UITEST.ViewModel
 {
-    public class CreatePostPageViewModel
+    public class CreatePostPageViewModel : BaseViewModel
     {
         private readonly IRedditAPIConsumer _consumer;
+<<<<<<< HEAD
         private readonly Subreddit _currentSubreddit;
+=======
+        private Subreddit CurrentSubreddit;
+>>>>>>> 9366c7b5baff501d8a5edfeeb730de9c989ea2d2
 
         public delegate void PostSent();
-        public event PostSent PostSentEvent;
+        public event PostSent SentSuccesfulEvent;
+        public event PostSent SentUnsuccesfulEvent;
 
-        public CreatePostPageViewModel(IRedditAPIConsumer consumer, Subreddit subreddit)
+        public Subreddit currentSubreddit
+        {
+            get { return CurrentSubreddit; }
+            set {
+                if(value != CurrentSubreddit)
+                {
+                    CurrentSubreddit = value;
+                    OnPropertyChanged("CurrentSubreddit");
+                }
+            }
+        }
+
+
+        public CreatePostPageViewModel(IAuthenticationHelper helper, INavigationService service, IRedditAPIConsumer consumer) : base(service)
         {
             _consumer = consumer;
+<<<<<<< HEAD
             _currentSubreddit = subreddit;
+=======
+            _helper = helper;
+>>>>>>> 9366c7b5baff501d8a5edfeeb730de9c989ea2d2
         }
 
         public async Task CreateNewPostAsync(string title, string body = "")
         {
+<<<<<<< HEAD
             await _consumer.CreatePostAsync(_currentSubreddit, title, "self", body);
             PostSentEvent.Invoke();
+=======
+            var response = await _consumer.CreatePostAsync(CurrentSubreddit, title, "self", body);
+            if (response.Item1 == HttpStatusCode.OK)
+            {
+                SentSuccesfulEvent.Invoke();
+            }
+            else
+            {
+                SentUnsuccesfulEvent.Invoke();
+            }
+>>>>>>> 9366c7b5baff501d8a5edfeeb730de9c989ea2d2
         }
     }
 }
--- WepAPI/UITEST/ViewModel/MainPageViewModel.cs
+++ WepAPI/UITEST/ViewModel/MainPageViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using Entities.RedditEntities;
 using Gorilla.AuthenticationGorillaAPI;
 using Gorilla.Model;
+using Gorilla.View;
 using Model;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using UITEST.View;
 using Windows.Security.Credentials;
 using Windows.UI.Xaml.Controls;
@@ -14,9 +16,11 @@ namespace UITEST.ViewModel
 {
     public class MainPageViewModel : BaseViewModel, INotifyPropertyChanged
     {
+        public ICommand GoToCreatePostPageCommand { get; set; }
         bool firstTime = true;
         IRedditAPIConsumer _consumer;
         protected ISubredditRepository _repository;
+        public Subreddit subreddit;
         public ObservableCollection<Post> Posts { get; set; }
 
         public delegate void PostsReady();
@@ -34,11 +38,13 @@ namespace UITEST.ViewModel
             //}
             _repository = repository;
             _helper = helper;
+
+            GoToCreatePostPageCommand = new RelayCommand(o => _service.Navigate(typeof(CreatePostPage), subreddit));
         }
         
         public async void GeneratePosts()
         {
-            Subreddit subreddit = await _consumer.GetSubredditAsync("AskReddit");
+            subreddit = await _consumer.GetSubredditAsync("AskReddit");
             PostsReadyEvent.Invoke();
             Posts = subreddit.posts;
             OnPropertyChanged("Posts");
@@ -60,10 +66,7 @@ namespace UITEST.ViewModel
                 {
                     await Initialize();
                 }
-                
             }
-         
-
         }
     }
 }
