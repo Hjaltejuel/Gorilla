9a6dfea34c152cdd9ea2fcae1222ffb23b57c8f6
Mads Pedersen <madpe@itu.dk>
2017-12-14 17:10:35 +0100

    binding virker nu på profile page :)

--- WepAPI/UITEST/CustomUI/CustomButton.cs
+++ WepAPI/UITEST/CustomUI/CustomButton.cs
@@ -1,10 +1,4 @@
 ﻿using Entities.RedditEntities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UITEST.Model;
 using Windows.UI.Xaml.Controls;
 
 namespace UITEST.CustomUI
--- WepAPI/UITEST/ViewModel/PostPageViewModel.cs
+++ WepAPI/UITEST/ViewModel/PostPageViewModel.cs
@@ -1,13 +1,6 @@
 ﻿using Entities.RedditEntities;
 using Gorilla.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
 using System.Threading.Tasks;
-using UITEST.Model;
-using UITEST.View;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace UITEST.ViewModel
