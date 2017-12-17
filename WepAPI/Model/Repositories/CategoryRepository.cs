bf5ba1cb729c46b79240d58afef242705008c835
Rasmus Adrian Raarup <rraa@itu.dk>
2017-12-18 00:43:06 +0100

    new classes for categories

---
+++ WepAPI/Model/Repositories/CategoryRepository.cs
@@ -0,0 +1,66 @@
+﻿using Entities.GorillaAPI.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Exceptions;
+using Entities.GorillaEntities;
+
+namespace Model.Repositories
+{
+    class CategoryRepository : ICategoryRepository
+    {
+        private readonly IRedditDbContext _context;
+
+
+
+        public async Task<bool> GetAsync(string Username, string CategoryName)
+        {
+            var user = _context.Users.FindAsync(Username);
+            List<CategorySubreddit> listOfCategories = await (from a in _context.CategorySubreddits where a.Name == CategoryName select a).ToListAsync();
+
+            foreach (CategorySubreddit c in listOfCategories)
+            {
+                if(CategoryName == c.Name)
+                {
+                    _context.UserPreferences.Update(new UserPreference {Username = Username, SubredditName = CategoryName });
+                }
+            }
+            return true;
+           
+        }
+
+        private bool _disposedValue;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    _context.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                _disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~TrackRepository() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+    }
+}
