029218699ecaa035e0783cb573b0d4d49c2d6e6d
Jakob Roos <jroo@itu.dk>
2017-12-18 01:29:21 +0100

    login page test

--- WepAPI/BDSA2017.Assignment08.UWP.Tests/ViewModels/CreatePostPageViewModelTests.cs
+++ WepAPI/BDSA2017.Assignment08.UWP.Tests/ViewModels/CreatePostPageViewModelTests.cs
@@ -13,6 +13,7 @@ using Entities.RedditEntities;
 using UITEST.Model;
 using UITEST.Authentication.GorillaAuthentication;
 using UITEST.Model.RedditRestInterfaces;
+using XUnit;
 
 namespace BDSA2017.Assignment08.UWP.Tests.GorillaRepoTests
 {
@@ -30,7 +31,12 @@ namespace BDSA2017.Assignment08.UWP.Tests.GorillaRepoTests
             repository = new Mock<IRestUserPreferenceRepository>();
             consumer = new Mock<IRedditApiConsumer>();
 
-    }
+            _commentableViewModel = new CommentableViewModel(
+                _navigationService.Object,
+                _restUserPreferenceRepository.Object,
+                _redditApiConsumer.Object
+            );
+        }
         [Fact]
         public void Does_Post_Disliked_Call_Reddit()
         {
---
+++ WepAPI/BDSA2017.Assignment08.UWP.Tests/ViewModels/LoginPageViewModelTests.cs
@@ -0,0 +1,54 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Lib.Authentication;
+using UI.Lib.Authentication.GorillaAuthentication;
+using UI.Lib.Model;
+using UI.Lib.Model.GorillaRestInterfaces;
+using UI.Lib.Model.RedditRestInterfaces;
+using UI.Lib.ViewModel;
+using Xunit;
+
+namespace UI.Test.ViewModels
+{
+    public class LoginPageViewModelTests
+    {
+        private readonly Mock<INavigationService> _navigationService;
+        private readonly Mock<IRedditAuthHandler> _handler;
+        private readonly LoginPageViewModel _lpvm;
+        
+        public LoginPageViewModelTests()
+        {
+
+            _navigationService = new Mock<INavigationService>();
+            _handler = new Mock<IRedditAuthHandler>();
+            _lpvm = new LoginPageViewModel(
+                _navigationService.Object,
+                _handler.Object
+            );
+        }
+
+        [Fact(DisplayName = "Is authHandler called")]
+        public void Is_AuthHandler_And_HasAuthenticated_Called()
+        {
+            //Act
+            _lpvm.BeginAuthentication();
+
+            //Assert
+            _handler.Verify(v => v.BeginAuth(), Times.Once());
+        }
+
+        [Fact(DisplayName = "Is Logout called")]
+        public void Is_Logout_Called()
+        {
+            //Act
+            _lpvm.LogOut();
+
+            //Assert
+            _handler.Verify(v => v.LogOut(), Times.Once());
+        }
+    }
+}
