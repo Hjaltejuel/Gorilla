8e55b2cfdcb9739fc3e395ed0a137bf4bc4a567b
Mads Pedersen <madpe@itu.dk>
2017-12-18 08:45:04 +0100

    klasse til postpageviewmodel test

---
+++ WepAPI/BDSA2017.Assignment08.UWP.Tests/ViewModels/PostPageViewModelTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Lib.Model;
+using UI.Lib.Model.GorillaRestInterfaces;
+using UI.Lib.Model.RedditRestInterfaces;
+using UI.Lib.ViewModel;
+using xunit;
+
+namespace UI.Test.ViewModels
+{
+    public class PostPageViewModelTests
+    {
+        private readonly Mock<INavigationService> _navigationService;
+        private readonly Mock<IRestPostRepository> _restPostRepository;
+        private readonly Mock<IRestUserPreferenceRepository> _restUserPreferenceRepository;
+        private readonly Mock<IRedditApiConsumer> _redditApiConsumer;
+        private readonly Mock<IUserHandler> _userHandler;
+        private readonly PostPageViewModel _postPageViewModel;
+
+        public PostPageViewModelTests()
+        {
+            _navigationService = new Mock<INavigationService>();
+            _restPostRepository = new Mock<IRestPostRepository>();
+            _restUserPreferenceRepository = new Mock<IRestUserPreferenceRepository>();
+            _redditApiConsumer = new Mock<IRedditApiConsumer>();
+            _userHandler = new Mock<IUserHandler>();
+
+            _postPageViewModel = new PostPageViewModel(
+                _navigationService.Object,
+                _restPostRepository.Object,
+                _restUserPreferenceRepository.Object,
+                _redditApiConsumer.Object,
+                _userHandler.Object
+            );
+        }
+    }
+}
